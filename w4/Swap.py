5ae0c481d8add12f4744bbbf19f47fd849833e7c
Artem Aliev <artem.aliev@phystech.edu>
2020-10-12 14:40:37 +0300

    Ex 6

--- w4/Swap.py
+++ w4/Swap.py
@@ -1,15 +1,19 @@
 def swap(values):
+
     def wrapper(*args, **kwargs):
         args = reversed(args)
         values(*args, **kwargs)
+
     return wrapper
 
 
 @swap
 def div(x, y, show = False):
+
     res = x / y
     if show:
         print(res)
+        
     return res
 
 div(2, 4, show = True)
