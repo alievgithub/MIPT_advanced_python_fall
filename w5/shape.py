6db491229db9594da4c8eedd54703dfe99dd8e13
Artem Aliev <artem.aliev@phystech.edu>
2020-10-20 19:55:05 +0300

    Ex 4

---
+++ w5/shape.py
@@ -0,0 +1,21 @@
+class Shape:
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+class Triangle(Shape):
+
+    def area(self):
+         return self.width * self.height / 2
+
+class Rectangle(Shape):
+
+    def area(self):
+         return self.width * self.height
+         
+print('Enter width and heigth through a space: ', end = '')
+x, y = map(int, input().split())
+
+print('Triangle area is ', Triangle(x, y).area())
+print('Rectangle area is ', Rectangle(x, y).area())
