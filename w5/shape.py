4d2f0eaa1bf47d1da06e67162fbc11765dc17574
Artem Aliev <artem.aliev@phystech.edu>
2020-10-20 21:51:43 +0300

    Changed design

--- w5/shape.py
+++ w5/shape.py
@@ -1,19 +1,16 @@
 class Shape:
-
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
 class Triangle(Shape):
-
     def area(self):
          return self.width * self.height / 2
 
 class Rectangle(Shape):
-
     def area(self):
          return self.width * self.height
-         
+
 print('Enter width and heigth through a space: ', end = '')
 x, y = map(int, input().split())
 
